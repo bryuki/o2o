b333c3edc69bf9d63760b2fb30026ffe57ee9fe0
Bryuki <Bryuki@outlook.com>
2018-01-11 21:53:17 +0800

    删除多余文件,更新shopoperation.js

--- src/main/web/resources/js/shop/shopoperation.js
+++ src/main/web/resources/js/shop/shopoperation.js
@@ -4,40 +4,62 @@
 $(function() {
     var initUrl = '/o2o/shop/getshopinitinfo';
     var registerShopUrl = 'o2o/shopadmin/registershop';
+    alert(initUrl);
+    getShopInitInfo();
+
     function getShopInitInfo() {
-        $.getJSON(initUrl, function(data) {
+        $.getJSON(initUrl, function (data) {
             if (data.success) {
                 var tempHtml = '';
                 var tempAreaHtml = '';
-                data.shopCategoryList.map(function(item, index) {
+                data.shopCategoryList.map(function (item, index) {
                     tempHtml += '<option data-id="' + item.shopCategoryId + '">'
-                    + item.shopCategoryName + '</option>';
+                        + item.shopCategoryName + '</option>';
                 });
-                data.areaList.map(function(item, index) {
+                data.areaList.map(function (item, index) {
                     tempAreaHtml += '<option data-id="' + item.areaId + '">'
-                    + item.areaName + '</option>';
+                        + item.areaName + '</option>';
                 });
                 $('#shop-category').html(tempHtml);
                 $('#area').html(tempAreaHtml);
             }
         });
-        $('#submit').click(function() {
+        $('#submit').click(function () {
             var shop = {};
-            shop.shopName  = $('#shop-name').val();
+            shop.shopName = $('#shop-name').val();
             shop.shopAddr = $('#shop-addr').varl();
             shop.phone = $('shop-phone').val();
             shop.shopDesc = $('shop-desc').varl();
             shop.shopCategory = {
-                shopCategoryId : $('#shop-category').find('option').not(function() {
+                shopCategoryId: $('#shop-category').find('option').not(function () {
                     return !this.selected;
                 }).data('id')
             };
             shop.area = {
-                areaId: $('#area').find('option').not(function() {
+                areaId: $('#area').find('option').not(function () {
                     return !this.selected;
                 }).data('id')
             };
-            var shopImg = $('#shop')
-        }
+            var shopImg = $('#shop-img')[0].files[0];
+            var formData = new FormData();
+            formData.append('shopImg', shopImg);
+            formData.append('shopStr', JSON.Stringify(shop));
+            $.ajax({
+                url: registerShopUrl,
+                type: 'POST',
+                data: formData,
+                contentType: false,
+                processData: false,
+                cache: false,
+                success: function (data) {
+                    if (data.success) {
+                        $.toast('提交成功!');
+                    } else {
+                        $.toast('提交失败', data.errMsg);
+                    }
+                }
+
+            });
+        });
     }
 })
